6b96a6e583d525cfe501f3f3143886c70f7ff2d3
Pat0x <ivangx2232@gmail.com>
2026-05-09 20:46:15 -0300

    Update
    Añadido de funcion WrapF

--- battle-wing-project/Scripts/nave.gd
+++ battle-wing-project/Scripts/nave.gd
@@ -4,6 +4,7 @@ extends CharacterBody2D
 @export var vel_rotacion = 50
 @export var velocidad_maxima = 400
 
+@onready var tamano_pantalla = get_viewport_rect().size
 
 func Aceleracion():
 	var input_vector := Vector2(0, Input.get_action_strength("Acelerar"))
@@ -23,7 +24,13 @@ func Rotacion(delta):
 		rotate(-deg_to_rad(velocidad_maxima*delta))
 	pass
 
+func screen_wrap():
+	position.x = wrapf(position.x, 0, tamano_pantalla.x)
+	position.y = wrapf(position.y, 0, tamano_pantalla.y)
+	pass
+
 func _physics_process(delta: float) -> void:
 	Aceleracion()
 	Rotacion(delta)
+	screen_wrap()
 	move_and_slide()
