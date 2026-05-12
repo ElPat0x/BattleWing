62b11552cc1076ca5e480d78b49b37dcd2fb1730
Pat0x <ivangx2232@gmail.com>
2026-05-11 21:33:42 -0300

    Script de vida y hitboxes (Incompleto)
    Añadido de la mecanica de vida y hitbox e hurtbox
    esta Incompleto por el momento

--- battle-wing-project/Scripts/bala.gd
+++ battle-wing-project/Scripts/bala.gd
@@ -5,9 +5,5 @@ extends Area2D
 func _process(delta: float) -> void:
 	position += transform.y * velocidad * delta
 
-
-
 func borrar_bala() -> void:
 	queue_free()
-	print("bala borrada")
-	pass # Replace with function body.
--- battle-wing-project/Scripts/nave.gd
+++ battle-wing-project/Scripts/nave.gd
@@ -3,11 +3,11 @@ extends CharacterBody2D
 @export var aceleracion = 7.5
 @export var vel_rotacion = 150
 @export var velocidad_maxima = 400
-@export var vida = 6
-
+@export var vida:int = 5
 
 @onready var bala = preload("res://Scenes/Objetos/bala.tscn")
 @onready var tamano_pantalla = get_viewport_rect().size
+@onready var canion = $canion
 
 func Aceleracion():
 	var input_vector := Vector2(0, Input.get_action_strength("Acelerar"))
@@ -27,23 +27,41 @@ func Rotacion(delta):
 		rotate(-deg_to_rad(vel_rotacion*delta))
 	pass
 
-func screen_wrap():
-	position.x = wrapf(position.x, 0, tamano_pantalla.x)
-	position.y = wrapf(position.y, 0, tamano_pantalla.y)
-	pass
-
-func _physics_process(delta: float) -> void:
-	Aceleracion()
-	Rotacion(delta)
-	disparar()
-	screen_wrap()
-	move_and_slide()
-
-
 func disparar():
 	
 	if Input.is_action_just_pressed("Disparar"):
 		var spawnear_bala = bala.instantiate()
 		get_tree().root.add_child(spawnear_bala)
-		spawnear_bala.global_position = global_position
+		spawnear_bala.global_position = canion.global_position
 		spawnear_bala.rotation = rotation
+
+func screen_wrap():
+	position.x = wrapf(position.x, 0, tamano_pantalla.x)
+	position.y = wrapf(position.y, 0, tamano_pantalla.y)
+	pass
+
+
+
+func _physics_process(delta: float) -> void:
+	Aceleracion()
+	Rotacion(delta)
+	disparar()
+	
+	screen_wrap()
+	move_and_slide()
+
+func recibir_danio() -> void:
+	vida -= 1
+	print("daño recibido")
+	print(vida)
+	if vida < 0:
+		queue_free()
+		print("borrando")
+
+
+
+func _on_hurtbox_area_entered(area: Area2D) -> void:
+	if area.name == "Bala":
+		print("He detectado una bala")
+		recibir_danio()
+	pass # Replace with function body.
